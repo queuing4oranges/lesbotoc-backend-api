1468f340b3b9bb92e4b57f8c87e26ee1f7fb42b2
queuing4oranges <katjazenker@gmail.com>
2022-12-19 20:40:01 +0100

    corrected single read image

--- images/single_pic.php
+++ images/single_pic.php
@@ -1,8 +1,9 @@
 <?php
-//displaying all images
+//displaying one image
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
+header("Content-Type: application/json");
 
 include 'DbConnection.php';
 
@@ -14,12 +15,12 @@ $method = $_SERVER['REQUEST_METHOD'];
 if ($method === "GET") {
 
     $path = explode('/', $_SERVER['REQUEST_URI']);
-    if (isset($path[3]) && is_numeric($_path[3])) {
+    if (isset($path[3]) && is_numeric($path[3])) {
         $sql = "SELECT * FROM images WHERE id=:id";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':id', $path[3]);
         $stmt->execute();
-        $events = $stmt->fetch(PDO::FETCH_ASSOC);
+        $images = $stmt->fetch(PDO::FETCH_ASSOC);
         echo json_encode($images);
     } else {
         echo "error in retrieving single image";
